b3ed4261d6afebac2567a3837ac02f6ce34bdc9a
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-07-27 14:45:17 +0600

    feat: Add directory menu animation and effects

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,12 +3,13 @@ import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`menu-item ${size}`}
-    >
+    <div className={`menu-item ${size}`}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      ></div>
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
