d27e4af7ae3dd4dd1cddeb64f29539f76b5e0566
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-07-30 23:57:50 +0600

    feat: Add header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./pages/header/header.component";
 import HomePage from "./pages/homepage/hompage.component";
 import Shop from "./pages/shop/shop.component";
 import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route exact path="/shop">
-            <Shop />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/shop">
+          <Shop />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
