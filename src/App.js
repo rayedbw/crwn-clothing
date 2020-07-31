de3f3bd995135e92bc583eeb1504dc808707eaa4
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-07-31 11:25:18 +0600

    feat: Add Sign In page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Header from "./pages/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import HomePage from "./pages/homepage/hompage.component";
 import Shop from "./pages/shop/shop.component";
+
 import "./App.css";
 
 const App = () => {
@@ -15,6 +18,9 @@ const App = () => {
         </Route>
         <Route exact path="/shop">
           <Shop />
+        </Route>
+        <Route exact path="/sign-in">
+          <SignInAndSignUp />
         </Route>
       </Switch>
     </Router>
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button-component";
+
+import "./sign-in.styles.scss";
+
+const useSignInInput = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    if (name === "email") {
+      setEmail(value);
+    } else {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return [email, password, handleChange, handleSubmit];
+};
+
+const SignIn = () => {
+  const [email, password, handleChange, handleSubmit] = useSignInInput();
+
+  return (
+    <div className="sign-in">
+      <h1>I already have an account</h1>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          type="email"
+          name="email"
+          value={email}
+          handleChange={handleChange}
+          required
+          label="Email"
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          required
+          label="Password"
+        />
+        <CustomButton type="submit">Sign In</CustomButton>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
